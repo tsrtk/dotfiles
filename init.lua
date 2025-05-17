ae77ffdda23834e76313b8758deaaf7dabe45210
tsrtk <52724019+tsrtk@users.noreply.github.com>
2025-05-18 05:22:18 +0900

    Add files via upload
    Put the init.lua into ~/.config/nvim

---
+++ init.lua
@@ -0,0 +1,68 @@
+-- Basic
+vim.opt.number = true
+
+-- Copy and Paste
+vim.opt.clipboard = "unnamedplus"
+
+vim.keymap.set('n', '<C-c>', 'y', { noremap = true })
+vim.keymap.set('v', '<C-c>', 'y', { noremap = true })
+vim.keymap.set('n', '<C-v>', '"+p', { noremap = true })
+vim.keymap.set('v', '<C-v>', '"+p', { noremap = true })
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    vim.fn.system({
+        "git", "clone", "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", lazypath
+    })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  -- Configuring LSP
+  "neovim/nvim-lspconfig",
+
+  -- Auto-completion
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+      "hrsh7th/cmp-vsnip", -- For `vsnip` users
+      "hrsh7th/vim-vsnip"
+    }
+  }
+})
+
+-- Configuring LSP
+local lspconfig = require('lspconfig')
+lspconfig.pyright.setup{}
+vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>')
+vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>')
+vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>')
+
+-- Auto-completion
+local cmp = require'cmp'
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'vsnip' }, -- Snippet support
+  }, {
+    { name = 'buffer' },
+  })
+})
